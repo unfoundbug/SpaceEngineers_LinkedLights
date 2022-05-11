aaf9f76c7476ed4839080b946bd54f4df26823f0
unfoundbug <iam@theadmin.co.uk>
2022-05-11 18:24:09 +0100

    Stylecop addition and cleanup

---
+++ Data/Scripts/LinkedLights/LightHookHelper.cs
@@ -0,0 +1,146 @@
+﻿// <copyright file="LightHookHelper.cs" company="UnFoundBug">
+// Copyright (c) UnFoundBug. All rights reserved.
+// </copyright>
+
+namespace TestScript
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Sandbox.Game.EntityComponents;
+    using Sandbox.ModAPI;
+    using Sandbox.ModAPI.Interfaces.Terminal;
+    using VRage.ModAPI;
+    using VRage.Utils;
+
+    /// <summary>
+    /// Static container of helper functions.
+    /// </summary>
+    public static class LightHookHelper
+    {
+        private static bool controlsInit = false;
+        private static IMyTerminalControlSeparator separator;
+        private static IMyTerminalControlListbox listControl;
+
+        private static Guid StorageGuid => new Guid("{F4D66A79-0469-47A3-903C-7964C8F65A25}");
+
+        /// <summary>
+        /// Gets the Stored mod value as a long.
+        /// </summary>
+        /// <param name="source">Source block.</param>
+        /// <returns>Storage converted to long with null checking.</returns>
+        public static long GetTargetId(IMyLightingBlock source)
+        {
+            long targetBlockId = 0;
+            if (source?.Storage?.ContainsKey(StorageGuid) ?? false)
+            {
+                targetBlockId = long.Parse(source.Storage.GetValue(StorageGuid));
+            }
+
+            return targetBlockId;
+        }
+
+        /// <summary>
+        /// Sets the stored value for a block in the mod.
+        /// </summary>
+        /// <param name="target">Target block.</param>
+        /// <param name="value">New EntityId.</param>
+        public static void SetTargetId(IMyLightingBlock target, long value)
+        {
+            if (target.Storage == null)
+            {
+                target.Storage = new MyModStorageComponent();
+            }
+
+            target.Storage.SetValue(StorageGuid, value.ToString());
+        }
+
+        /// <summary>
+        /// Attach controls to light terminal menus.
+        /// </summary>
+        public static void AttachControls()
+        {
+            if (!controlsInit)
+            {
+                controlsInit = true;
+            }
+            else
+            {
+                return;
+            }
+
+            // Logging.Instance.WriteLine("Light hook controls for " + typeof(IMyLightingBlock).Name + " started.");
+            separator = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlSeparator, IMyLightingBlock>("lightlink_seperator");
+            separator.Enabled = (lb) => true;
+            separator.Visible = (lb) => true;
+
+            listControl = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlListbox, IMyLightingBlock>("lightlink_block");
+            listControl.Title = MyStringId.GetOrCompute("Linked Block");
+            listControl.Tooltip = MyStringId.GetOrCompute("If a block is selected here, the lights enable/disable will be bound to the selected block");
+            listControl.SupportsMultipleBlocks = false;
+            listControl.Visible = block => true;
+            listControl.VisibleRowsCount = 8;
+            listControl.ItemSelected = (block, selected) =>
+            {
+                // Logging.Instance.WriteLine("Light item selected: " + block.DisplayNameText + "has selected:  " + selected.Count.ToString());
+                var localLight = (IMyLightingBlock)block;
+                if (localLight != null && selected.Count != 0)
+                {
+                    string selectedTarget = ((long)selected.FirstOrDefault().UserData).ToString();
+
+                    // Logging.Instance.WriteLine("Light: " + localLight.DisplayNameText + "has new value set: " + selectedTarget);
+                    SetTargetId(localLight, (long)(selected.FirstOrDefault()?.UserData ?? 0));
+
+                    // Logging.Instance.WriteLine("Value set");
+                    localLight.NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+                }
+            };
+            listControl.ListContent = (block, items, selected) =>
+            {
+                // Logging.Instance.WriteLine("List content building!");
+                var localLight = block.GameLogic.GetAs<IMyLightingBlock>();
+
+                // Logging.Instance.WriteLine("Source block as lightsource: " + localLight == null ? "NULL" : "Not Null");
+                items.Add(new MyTerminalControlListBoxItem(
+                    MyStringId.GetOrCompute("None"),
+                    MyStringId.GetOrCompute("None"),
+                    0));
+
+                // Logging.Instance.WriteLine("Added None Entry");
+                List<IMyFunctionalBlock> foundBlockList = new List<IMyFunctionalBlock>();
+                var funcBlocks = block.CubeGrid.GetFatBlocks<IMyFunctionalBlock>();
+
+                // Logging.Instance.WriteLine("Found " + funcBlocks.ToList().Count + " functional block sources");
+                long targetId = LightHookHelper.GetTargetId(block as IMyLightingBlock);
+                foreach (var funcBlock in funcBlocks)
+                {
+                    var newItem = new MyTerminalControlListBoxItem(
+                        MyStringId.GetOrCompute(funcBlock.DisplayNameText),
+                        MyStringId.GetOrCompute(funcBlock.Name),
+                        funcBlock.EntityId);
+                    if (funcBlock.EntityId == targetId)
+                    {
+                        // Logging.Instance.WriteLine("Setting " + funcBlock.DisplayNameText + "to selected");
+                        selected.Add(newItem);
+                    }
+
+                    items.Add(newItem);
+                }
+
+                if (selected.Count == 0)
+                {
+                    selected.Add(items[0]);
+                }
+
+                listControl.RedrawControl();
+
+                // Logging.Instance.WriteLine("Redrawn");
+            };
+
+            MyAPIGateway.TerminalControls.AddControl<IMyLightingBlock>(separator);
+            MyAPIGateway.TerminalControls.AddControl<IMyLightingBlock>(listControl);
+            MyAPIGateway.TerminalControls.AddControl<IMyReflectorLight>(separator);
+            MyAPIGateway.TerminalControls.AddControl<IMyReflectorLight>(listControl);
+        }
+    }
+}
--- Data/Scripts/LinkedLights/ReflectorLightHooks.cs
+++ Data/Scripts/LinkedLights/ReflectorLightHooks.cs
@@ -1,115 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using ParallelTasks;
-using VRage.Game.Components;
-using Sandbox.Common.ObjectBuilders;
-using Sandbox.Game.EntityComponents;
-using Sandbox.ModAPI;
-using VRage.ObjectBuilders;
-using VRage.ModAPI;
-using Sandbox.ModAPI.Interfaces.Terminal;
-using TestScript;
-using VRage.Game.ModAPI;
-using VRage.Utils;
-using IMyFunctionalBlock = Sandbox.ModAPI.IMyFunctionalBlock;
-using IMyLightingBlock = Sandbox.ModAPI.IMyLightingBlock;
-using IMyTerminalBlock = Sandbox.ModAPI.IMyTerminalBlock;
+﻿// <copyright file="ReflectorLightHooks.cs" company="UnFoundBug">
+// Copyright (c) UnFoundBug. All rights reserved.
+// </copyright>
 
 namespace UnFoundBug.LightLink
 {
-    //here you can use any objectbuiler e.g. MyObjectBuilder_Door, MyObjectBuilder_Decoy
+    using Sandbox.Common.ObjectBuilders;
+    using TestScript;
+    using VRage.Game.Components;
+
+    /// <summary>
+    /// Hooks for MyObjectBuilder_ReflectorLight.
+    /// </summary>
     [MyEntityComponentDescriptor(typeof(MyObjectBuilder_ReflectorLight), true)]
-    public class ReflectorLightHooks : MyGameLogicComponent
+    public class ReflectorLightHooks : BaseLightHooks
     {
-        private IMyLightingBlock BaseLight => (IMyLightingBlock)this.Entity;
-
-        private IMyFunctionalBlock targetBlock = null;
-        
-        //if you subscribed to events, please always unsubscribe them in close method 
-        public override void Close()
-        {
-        }
-
-        public ReflectorLightHooks()
-        {
-            //Logging.Instance.WriteLine("MyObjectBuilder_InteriorLightConstructed!");
-            BaseLightHooks.AttachControls();
-        }
-
-        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
-        {
-            //Logging.Instance.WriteLine("MyObjectBuilder Init started");
-            base.Init(objectBuilder);
-            Entity.NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
-            Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
-        }
-
-        public override void UpdateOnceBeforeFrame()
-        {
-            base.UpdateOnceBeforeFrame();
-            //Logging.Instance.WriteLine("Light update for " + this.BaseLight.DisplayNameText + " started.");
-
-            long targetBlockId = BaseLightHooks.GetTargetId(this.BaseLight);
-            //Logging.Instance.WriteLine("Found target block: " + targetBlockId.ToString());
-            if (targetBlockId != 0)
-            {
-                var funcBlocks = this.BaseLight.CubeGrid.GetFatBlocks<IMyFunctionalBlock>();
-                var target = funcBlocks.FirstOrDefault(found => found.EntityId == targetBlockId);
-                AttachTarget(target);
-            }
-            else
-            {
-                DetachFromTarget();
-            }
-
-        }
-
-        public override void UpdateBeforeSimulation10()
-        {
-            base.UpdateBeforeSimulation10();
-            if(targetBlock!=null)
-                if (targetBlock is IMyShipToolBase)
-                {
-                    var asTool = targetBlock as IMyShipToolBase;
-                    var newEn = asTool.Enabled | asTool.IsActivated;
-                    if (this.BaseLight.Enabled != newEn)
-                    {
-                        this.BaseLight.Enabled = newEn;
-                    }
-                }
-                else
-                {
-                    //Logging.Instance.WriteLine(this.BaseLight.DisplayNameText + "is changing to " + this.targetBlock.Enabled.ToString());
-                    this.BaseLight.Enabled = this.targetBlock.Enabled;
-                }
-        }
-
-        private void DetachFromTarget()
-        {
-            if (targetBlock != null)
-            {
-                //Logging.Instance.WriteLine(this.BaseLight.DisplayNameText + "is now detached from " + this.targetBlock.DisplayNameText);
-                targetBlock.OnMarkForClose -= TargetBlock_OnMarkForClose;
-                targetBlock = null;
-            }
-        }
-
-        private void AttachTarget(IMyFunctionalBlock newTarget)
-        {
-            //DetachFromTarget();
-            if (newTarget != null)
-            {
-                this.targetBlock = newTarget;
-                var targetAsTool = targetBlock as IMyShipToolBase;
-                this.targetBlock.OnMarkForClose += TargetBlock_OnMarkForClose;
-            }
-        }
-
-        private void TargetBlock_OnMarkForClose(IMyEntity obj)
-        {
-            DetachFromTarget();
-        }
     }
 }
